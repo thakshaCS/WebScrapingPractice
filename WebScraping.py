c77430cc3b56d4250402d7a0be497fa5b9588488
Thaksha <thaksha.mangaleswaran@mail.utoronto.ca>
2022-01-17 22:18:28 -0500

    fixed bugs

--- WebScraping.py
+++ WebScraping.py
@@ -1,14 +1,14 @@
-import bs4
-from urllib.request import urlopen
+
+import urllib.request
 from bs4 import BeautifulSoup
 
 website_url = "https://www.newegg.ca/p/pl?d=graphics+cards"
-uClient = urlopen(website_url)  # downloads webpage
+uClient = urllib.request.urlopen(website_url)  # downloads webpage
 page_html = uClient.read()  # offloads content into variable
 uClient.close()
 page_soup = BeautifulSoup(page_html, "html.parser")  # parse file as html file
 # page_soup.h1 # grabs header
-containers = page_soup.findAll("div", {"class": "item-container"})  # grabs all
+containers = page_soup.findAll("div", {"class":"item-container"})  # grabs all
 # div that have the class item-container, returned as a list
 
 
@@ -18,12 +18,13 @@ f = open(filename, "w")
 
 headers = "brand, product_name, shipping\n"
 f.write(headers)
+print(containers[0].a.img["title"])
 for container in containers:
-    brand = container.div.div.a.img["title"]  # index as if its a dictionary
-    # to grab an # attribute
-    title_container = container.findAll("a", {"class": "item-title"})
+    brand_container = container.findAll("div", {"class": "item-branding"})
+    brand = brand_container[0].a.img["title"]  # index as if its a dictionary  to grab an  attribute
+    title_container = container.findAll("a", {"class":"item-title"})
     product_name = title_container[0].text
-    shipping_container = container.findAll("li", {"class": "price-ship"})
+    shipping_container = container.findAll("li", {"class":"price-ship"})
     shipping = shipping_container[0].text.strip()
 
     print("brand: " + brand)
