391ba558482ca3ff986376e2630573591d8457b0
Thaksha <thaksha.mangaleswaran@mail.utoronto.ca>
2022-01-17 21:32:22 -0500

    finished code

--- WebScraping.py
+++ WebScraping.py
@@ -8,13 +8,27 @@ page_html = uClient.read()  # offloads content into variable
 uClient.close()
 page_soup = BeautifulSoup(page_html, "html.parser")  # parse file as html file
 # page_soup.h1 # grabs header
-containers = page_soup.findAll("div", {"class":"item-container"}) # grabs all
+containers = page_soup.findAll("div", {"class": "item-container"})  # grabs all
 # div that have the class item-container, returned as a list
 
+
+# open a file
+filename = "products.csv"
+f = open(filename, "w")
+
+headers = "brand, product_name, shipping\n"
+f.write(headers)
 for container in containers:
-    brand = container.div.div.a.img["title"] # index as if its a dictionary
+    brand = container.div.div.a.img["title"]  # index as if its a dictionary
     # to grab an # attribute
-    title_container = container.findAll("a", {"class":"item-title"})
+    title_container = container.findAll("a", {"class": "item-title"})
     product_name = title_container[0].text
-    shipping_container = container.findAll("li", {"class":"price-ship"})
+    shipping_container = container.findAll("li", {"class": "price-ship"})
     shipping = shipping_container[0].text.strip()
+
+    print("brand: " + brand)
+    print("name: " + product_name)
+    print("shipping: " + shipping)
+    f.write(
+        brand + "," + product_name.replace(",", "|") + "," + shipping + "\n")
+f.close()
